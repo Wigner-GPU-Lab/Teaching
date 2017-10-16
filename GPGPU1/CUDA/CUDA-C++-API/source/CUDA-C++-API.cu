a8f8d547a749cd66392473fa0ed9d6b997f21a38
Nagy-Egri Máté Ferenc <nagy-egri.mate@wigner.mta.hu>
2017-10-16 15:26:12 +0200

    Working

--- GPGPU1/CUDA/CUDA-C++-API/source/CUDA-C++-API.cu
+++ GPGPU1/CUDA/CUDA-C++-API/source/CUDA-C++-API.cu
@@ -5,7 +5,7 @@ int main(void)
 {
   int N = 1<<20;
   std::vector<float> x(N, 1.0f), y(N, 2.0f);
-  float *d_x = nullptr, *d_y = nullptr;
+  float *d_x = nullptr, *d_y = nullptr, a = 2.0;
 
   cudaMalloc(&d_x, N*sizeof(float));
   cudaMalloc(&d_y, N*sizeof(float));
@@ -13,10 +13,11 @@ int main(void)
   cudaMemcpy(d_x, x.data(), N*sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(d_y, y.data(), N*sizeof(float), cudaMemcpyHostToDevice);
 
-  auto saxpy = [=, a = 2.0f] __device__ ()
+  auto saxpy = [=] __device__ ()
   {
       int i = blockIdx.x*blockDim.x + threadIdx.x;
-      d_y[i] = 6;// a * d_x[i] + d_y[i];
+      
+      if (i < N) d_y[i] = a * d_x[i] + d_y[i];
   };
 
   // Perform SAXPY on 1M elements
@@ -24,13 +25,16 @@ int main(void)
 
   cudaMemcpy(y.data(), d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
 
-  float maxError = *std::max_element(y.cbegin(), y.cend(),
-                                    [res = 4.0](const float& lhs,
-                                                const float& rhs)
+  float res = a * 1.0f + 2.0f;
+  float maxError = *std::max_element(y.cbegin(),
+                                     y.cend(),
+                                     [=](const float& lhs, const float& rhs)
   {
     return std::abs(lhs - res) < std::abs(rhs - res);
   });
-  std::cout << "Max error: "<< maxError << std::endl;
+
+  std::cout.precision(16);
+  std::cout << "Max error: " << std::abs(maxError - res) << std::endl;
 
   cudaFree(d_x);
   cudaFree(d_y);
