085f29f04d6acf561b961503d62048537de1bad8
u235axe <u235axe@gmail.com>
2018-07-02 11:18:05 +0200

    Fixed bug in thread launch iterator slicing.

--- CppSampleCodes/multithread_average.cpp
+++ CppSampleCodes/multithread_average.cpp
@@ -12,7 +12,7 @@ int main()
 	std::vector<double> vec(10'000'000);
 
 	std::random_device random_source;
-    std::normal_distribution<double> dist(5.0, 10.0); // normal distribution with average 5 and sigma = 10, generating doubles
+	std::normal_distribution<double> dist(5.0, 10.0); // normal distribution with average 5 and sigma = 10, generating doubles
 	std::generate( vec.begin(), vec.end(), [&](){ return dist(random_source); } ); //fill the vector with random numbers
 
 	auto averager = [](auto it0, auto it1)//lambda to average numbers between two iterators
@@ -23,17 +23,16 @@ int main()
 	};
 
 	int max_num_of_threads = (int)std::thread::hardware_concurrency(); //query number of threads
-    std::cout << "Using " << max_num_of_threads << " threads.\n";
+	std::cout << "Using " << max_num_of_threads << " threads.\n";
 	std::vector<std::future<double>> futures(max_num_of_threads);
 	
 	auto time0 = std::chrono::high_resolution_clock::now();
 
 	//start threads:
-	auto step = 1 + (int)vec.size() / max_num_of_threads;
 	for(int n=0; n<max_num_of_threads; ++n )
 	{
-		auto it0 = std::next(vec.begin(), std::max( n    * step, 0)              );
-		auto it1 = std::next(vec.begin(), std::min((n+1) * step, (int)vec.size()));
+		auto it0 = vec.begin() + k * vec.size() / max_num_of_threads;
+        	auto it1 = vec.begin() + (k + 1) * vec.size() / max_num_of_threads;
 		futures[n] = std::async( std::launch::async, averager, it0, it1 );
 	}
 
@@ -42,9 +41,9 @@ int main()
 
 	auto time1 = std::chrono::high_resolution_clock::now();
 
-    auto serial_result = std::accumulate(vec.begin(), vec.end(), 0.0);
-    auto time2 = std::chrono::high_resolution_clock::now();
+	auto serial_result = std::accumulate(vec.begin(), vec.end(), 0.0);
+	auto time2 = std::chrono::high_resolution_clock::now();
 
 	std::cout << "Serial average is:   " << serial_result / (double)vec.size() << " \t Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(time2-time1).count() << " usec.\n";
-    std::cout << "Parallel average is: " << parallel_result / (double)max_num_of_threads << " \t Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(time1-time0).count() << " usec\n";
+	std::cout << "Parallel average is: " << parallel_result / (double)max_num_of_threads << " \t Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(time1-time0).count() << " usec\n";
 }
