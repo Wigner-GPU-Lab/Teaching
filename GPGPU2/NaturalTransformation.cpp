605fa6154af121e25a92528ad2265b6238523679
u235axe <u235axe@gmail.com>
2018-10-11 19:47:59 +0200

    Update natural transformation to std::optional
    workaround MSVC ambiguity error.

--- GPGPU2/NaturalTransformation.cpp
+++ GPGPU2/NaturalTransformation.cpp
@@ -1,60 +1,58 @@
-
 #include <type_traits>
-#include <memory>
+#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <optional>
 
-struct Just {};
-struct Nothing {};
+template<typename A> using Maybe = std::optional<A>;
 
-template<typename T> struct Maybe
-{
-	std::unique_ptr<T> p;
+//helper base overload #clang bug
+template<template<typename...> typename Functor, typename F, typename A, typename B = typename std::result_of<F(A)>::type>
+Functor<B> fmap_impl(F f, Functor<A> in){ return fmap_impl(f, in); }
 
-	Maybe(Nothing) :p{ nullptr } {}
-	Maybe(Just, T value_in) :p{ new T{ value_in } } {}
-	Maybe(Maybe const& cpy) : p{ cpy.is_valid() ? new T{ cpy.value() } : nullptr } {}
-
-	T    value()    const { return *p; }
-	bool is_valid() const { return p == nullptr ? false : true; }
-};
+//fmap wrapper as function object
+inline auto fmap = [](auto func, auto functor){ return fmap_impl(func, functor); };
 
 //fmap implementation as a function template
 template<typename F, typename A, typename B = typename std::result_of<F(A)>::type>
 Maybe<B> fmap_impl(F f, Maybe<A> in)
 {
-	return in.is_valid() ? Maybe<B>{ Just(), f(in.value()) } : Maybe<B>{ Nothing() };
+    return in.has_value() ? Maybe<B>{ f(in.value()) } : Maybe<B>{ };
 }
+
+//Workaround fmap_impl predeclaration ambiguity in MSVC
+template<typename A>
+struct VectorImpl
+{
+	typedef std::vector<A> type;
+};
+template<typename A> using Vector = typename VectorImpl<A>::type;
 
 //fmap implementation for std::vector
 template<typename F, typename A, typename B = typename std::result_of<F(A)>::type>
-std::vector<B> fmap_impl(F f, std::vector<A> in)
+Vector<B> fmap_impl(F f, Vector<A> in)
 {
-    std::vector<B> res(in.size());
+    Vector<B> res(in.size());
     std::transform(in.cbegin(), in.cend(), res.begin(), f);
     return res;
 }
 
-//fmap wrapper as function object
-auto fmap = [](auto func, auto functor) { return fmap_impl(func, functor); };
-
 //Natural transformation, valid for all X!
 template<typename X>
-std::vector<X> nt_Maybe_to_vector(Maybe<X> m)
+Vector<X> nt_Maybe_to_vector(Maybe<X> m)
 {
-    return m.is_valid() ? std::vector<X>{ m.value() } : std::vector<X>{};
+    return m.has_value() ? Vector<X>{ m.value() } : Vector<X>{};
 };
-
 
 int main()
 {
     auto is_even = [](int x){ return x % 2 == 0; };
 
-    auto m = Maybe<int>{ Just(), 5 };
+    auto m = Maybe<int>{ 5 };
    
     //the following two ways get the same result:
     auto result1 = fmap(is_even, nt_Maybe_to_vector( m ) );
     auto result2 = nt_Maybe_to_vector( fmap(is_even, m ) );
 
-    printf("%s %s\n", result1[0] ? "true" : "false", result2[0] ? "true" : "false");
+    std::cout << (result1[0] ? "true" : "false") << "   " << (result2[0] ? "true" : "false") << "\n";
 }
