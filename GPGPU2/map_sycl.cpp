e13406e743937128cf4c0934046251cb1f91923d
u235axe <u235axe@gmail.com>
2018-12-03 16:53:41 +0100

    Update sample.

--- GPGPU2/map_sycl.cpp
+++ GPGPU2/map_sycl.cpp
@@ -29,8 +29,11 @@ int main()
 		h_dst[i] = 0;
 	}
 
+	try
 	{
-		cl::sycl::queue queue{ cl::sycl::gpu_selector() };
+		cl::sycl::queue queue{cl::sycl::intel_selector()};
+		std::cout << "Selected platform: " << queue.get_context().get_platform().get_info<cl::sycl::info::platform::name>() << "\n";
+		std::cout << "Selected device:   " << queue.get_device().get_info<cl::sycl::info::device::name>() << "\n";
 
 		cl::sycl::buffer<T, 1> b_src(h_src.data(), n);
 		cl::sycl::buffer<T, 1> b_dst(h_dst.data(), n);
@@ -42,14 +45,17 @@ int main()
 		queue.submit([&](cl::sycl::handler& cgh)
 		{
 			auto a_src = b_src.get_access<cl::sycl::access::mode::read>(cgh);
-			auto a_dst = b_dst.get_access<cl::sycl::access::mode::write>(cgh);
+			auto a_dst = b_dst.get_access<cl::sycl::access::mode::discard_write>(cgh);
 
 			cgh.parallel_for<class Map>(r, [=](cl::sycl::id<1> i)
 			{
 				map(sq, a_src, a_dst, i);
 			});
 		});
+
+		queue.wait();
 	}
+	catch (cl::sycl::exception e){ std::cout << "Exception encountered in SYCL: " << e.what() << "\n"; return -1; }
 
 	for(int i=0; i<n; i++)
 	{
