b1f2e642786649702c6cea9497498c926a18e099
u235axe <u235axe@gmail.com>
2018-12-03 19:26:43 +0100

    Updated sample to SYCL 1.2.1, currently not compiles due to ComputeCpp issues

--- GPGPU2/reduce_map_idx_sycl.cpp
+++ GPGPU2/reduce_map_idx_sycl.cpp
@@ -12,7 +12,7 @@ template<typename T>
 auto sq(T const& x){ return x*x; }
 
 template<typename Fr, typename Fm, typename R = std::result_of_t<Fm(size_t)>>
-R reduce_map_idx(Fr fr, Fm fm, size_t n)
+R reduce_map_idx(cl::sycl::queue& queue, Fr fr, Fm fm, size_t n)
 {
 	//block size, assumes input > n
 	size_t local_count = 16;
@@ -20,29 +20,26 @@ R reduce_map_idx(Fr fr, Fm fm, size_t n)
 	std::vector<R> res( (size_t)std::ceil(n/2.0/local_count) );
 
 	{
-		cl::sycl::queue queue{ cl::sycl::gpu_selector() };
-
 		cl::sycl::buffer<R, 1> b_dst(res.data(), res.size()); 
 
 		cl::sycl::nd_range<1> r(n/2, local_count);
 
 		queue.submit([&](cl::sycl::handler& cgh)
 		{
-			cl::sycl::accessor<R, 1, cl::sycl::access::mode::read_write,
-				cl::sycl::access::target::local> tmp(cl::sycl::range<1>(local_count), cgh);
+			cl::sycl::accessor<R, 1, cl::sycl::access::mode::read_write, cl::sycl::access::target::local> tmp(cl::sycl::range<1>(local_count), cgh);
 
-			auto dst  = b_dst.template get_access<cl::sycl::access::mode::write>(cgh);
+			auto dst  = b_dst.template get_access<cl::sycl::access::mode::discard_write>(cgh);
 
 			cgh.parallel_for<class ReduceMapIdx>(r, [=](cl::sycl::nd_item<1> id)
 			{
-				auto g = id.get_group().get(0);
+				auto g = id.get_group().get_id();
 				auto bs = id.get_local_range().get(0);
-				auto l = id.get_local().get(0);
+				auto l = id.get_local_id().get(0);
 
 				auto i = 2 * (g * bs + l);
-
+#ifdef __SYCL_DEVICE_ONLY__
 				tmp[l] = fr( fm(i), fm(i+1) );
-
+#endif
 				id.barrier(cl::sycl::access::fence_space::local_space);
 
 				// do reduction in shared mem
@@ -66,31 +63,44 @@ R reduce_map_idx(Fr fr, Fm fm, size_t n)
 }
 
 template<typename F>
-auto integrate(F f, double L, size_t n)
+auto integrate(cl::sycl::queue queue, F f, double L, size_t n)
 {
 	auto f0 = [=](auto const& i0)
 	{
+#ifdef __SYCL_DEVICE_ONLY__
 		auto i = i0+1;
 		//w_i * f( x_i )
 		auto recnp1 = 1.0 / (n+1);
 		auto w = L*pi * recnp1 / sq( cl::sycl::sin(i*pi * recnp1) );
 		auto x = L / cl::sycl::tan( pi * i  * recnp1);
 		return w * f(x);
+#else
+		return 0.0;
+#endif
 	};
 
 	auto sum = [](auto const& x, auto const& y){ return x + y; };
 
-	return reduce_map_idx(sum, f0, n);
+	return reduce_map_idx(queue, sum, f0, n);
 }
 
 int main()
 {
 	auto f = [](auto const& x){ return cl::sycl::exp(-x*x); };
 
-	auto res = integrate(f, 1.5, 1024);
+	try
+	{
+		cl::sycl::queue queue{ cl::sycl::gpu_selector() };
+		std::cout << "Selected platform: " << queue.get_context().get_platform().get_info<cl::sycl::info::platform::name>() << "\n";
+		std::cout << "Selected device:   " << queue.get_device().get_info<cl::sycl::info::device::name>() << "\n";
 
-	//std::cout << "result = " << res << "\n";
-	printf("result    = %16.16f\n", res);
-	printf("reference = %16.16f\n", sqrt(pi));
+		auto res = integrate(queue, f, 1.5, 1024);
+
+		std::cout.precision(16);
+		std::cout << "result = " << res << "\n";
+		std::cout << "exact  = " << sqrt(pi) << "\n";
+	}
+	catch (cl::sycl::exception e){ std::cout << "Exception encountered in SYCL: " << e.what() << "\n"; return -1; }
+
 	return 0;
 }
