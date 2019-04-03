2e1c115e2f23b24dec469c061c3d96d8f272e373
Nagy-Egri Máté Ferenc <csiga.biga@aol.com>
2019-04-03 10:49:33 +0200

    Initial Conway files

---
+++ GPGPU1/SYCL-GL/Qt/Conway/include/SYCL-Conway.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+// NBody configure
+#include <SYCL-Conway-Config.hpp>
+
+// C++ behavioral defines
+#define _USE_MATH_DEFINES
+
+// Base class include
+#include <InteropWindow.hpp>
+
+// SYCL include
+#include <CL/sycl.hpp>
+
+// Graphics utility includes
+#include <QMatrix4x4>
+#include <QVector>
+
+// C++ includes
+#include <array>        // std::array
+#include <fstream>
+#include <memory>
+#include <future>
+#include <random>
+#include <memory>
+#include <sstream>
+#include <algorithm>
+#include <memory>       // std::unique_ptr
+
+
+namespace kernels { struct ConwayStep; }
+
+
+class Conway : public InteropWindow
+{
+    Q_OBJECT
+
+public:
+
+    explicit Conway(std::size_t plat_id,
+                    cl_bitfield dev_type,
+                    QWindow *parent = 0);
+    ~Conway() = default;
+
+    virtual void initializeGL() override;
+    virtual void initializeCL() override;
+    virtual void updateScene() override;
+    virtual void render() override;
+    virtual void render(QPainter* painter) override;
+    virtual void resizeGL(QResizeEvent* event_in) override;
+    virtual bool event(QEvent *event_in) override;
+
+private:
+
+    enum Buffer
+    {
+        Front = 0,
+        Back = 1
+    };
+
+    std::size_t dev_id;
+
+    // Host-side containers
+    std::vector<real4> pos_mass;
+    std::vector<real4> velocity;
+
+    // OpenGL related variables
+    std::unique_ptr<QOpenGLShader> vs, fs;
+    std::unique_ptr<QOpenGLShaderProgram> sp;
+    std::array<std::unique_ptr<QOpenGLBuffer>, 2> vbos;
+    std::array<std::unique_ptr<QOpenGLVertexArrayObject>, 2> vaos;
+
+	// OpenCL related variables
+    std::array<cl::BufferGL, 2> CL_posBuffs;
+    std::array<cl::Buffer, 2> CL_velBuffs;
+    std::vector<cl::Memory> interop_resources;  // Bloat
+    bool cl_khr_gl_event_supported;
+
+    // SYCL related variables
+    cl::sycl::context context;              // Context
+    cl::sycl::device device;                // Device
+    cl::sycl::queue compute_queue;          // CommandQueue
+
+	std::array<std::unique_ptr<cl::sycl::buffer<real4>>, 2> posBuffs;   // Simulation data buffers
+	std::array<std::unique_ptr<cl::sycl::buffer<real4>>, 2> velBuffs;   // Simulation data buffers
+
+	bool rightMouseButtonPressed;           // Variables to enable dragging
+	QPoint mousePos;                        // Variables to enable dragging
+    float dist, phi, theta;                 // Mouse polar coordinates
+    bool imageDrawn;                        // Whether image has been drawn since last iteration
+	bool needMatrixReset;                   // Whether matrices need to be reset in shaders
+
+	void mouseDrag(QMouseEvent* event_in);  // Handle mouse dragging
+	void mouseWheel(QWheelEvent* event_in); // Handle mouse wheel movement
+
+	void setMatrices();                     // Update shader matrices
+};
---
+++ GPGPU1/SYCL-GL/Qt/Conway/source/Main.cpp
@@ -0,0 +1,91 @@
+// Qt5 includes
+#include <QGuiApplication>
+#include <QMessageLogger>
+#include <QCommandLineParser>
+
+// Custom made includes
+#include <SYCL-Conway.hpp>
+
+// SYCL include
+#ifdef _MSC_VER 
+#pragma warning( push )
+#pragma warning( disable : 4310 ) // Prevents warning about cast truncates constant value
+#pragma warning( disable : 4100 ) // Prevents warning about unreferenced formal parameter
+#endif
+#include <CL/sycl.hpp>
+#ifdef _MSC_VER 
+#pragma warning( pop )
+#endif
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    QCoreApplication::setApplicationName("SYCL-GL Conway sample");
+    QCoreApplication::setApplicationVersion("1.0");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Sample application demonstrating OpenCL-OpenGL interop");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOptions({
+        {{"d", "device"}, "Device type to use", "[cpu|gpu|acc]", "cpu"},
+        {{"p", "platformId"}, "The index of the platform to use", "unsigned integral", "0"},
+        {{"x", "particles"}, "Number of particles", "unsigned integral", "8192"}
+    });
+
+    parser.process(app);
+
+    cl_bitfield dev_type = CL_DEVICE_TYPE_DEFAULT;
+    std::size_t plat_id = 0u, count = 8192u;
+    if(!parser.value("device").isEmpty())
+    {
+        if(parser.value("device") == "cpu")
+            dev_type = CL_DEVICE_TYPE_CPU;
+        else if(parser.value("device") == "gpu")
+            dev_type = CL_DEVICE_TYPE_GPU;
+        else if(parser.value("device") == "acc")
+            dev_type = CL_DEVICE_TYPE_ACCELERATOR;
+        else
+        {
+            qFatal("SYCL-Conway: Invalid device type: valid values are [cpu|gpu|acc]. Using CL_DEVICE_TYPE_DEFAULT instead.");
+        }
+    }
+    if(!parser.value("platformId").isEmpty())
+    {
+        plat_id = parser.value("platformId").toULong();
+    }
+    if(!parser.value("particles").isEmpty())
+    {
+        count = parser.value("particles").toULong();
+    }
+
+    NBody nbody(plat_id, dev_type, count);
+    nbody.setVisibility(QWindow::Maximized);
+    //nbody.setVisibility(QWindow::AutomaticVisibility);
+    //nbody.setWidth(1280);
+    //nbody.setHeight(720);
+    nbody.setMaxFPS(60);
+    //nbody.setMaxIPS(60);
+    nbody.setAnimating(true);
+
+    // Qt5 constructs
+    QSurfaceFormat my_surfaceformat;
+    
+    // Setup desired format
+    my_surfaceformat.setRenderableType(QSurfaceFormat::RenderableType::OpenGL);
+    my_surfaceformat.setProfile(QSurfaceFormat::OpenGLContextProfile::CoreProfile);
+    my_surfaceformat.setSwapBehavior(QSurfaceFormat::SwapBehavior::DoubleBuffer);
+    my_surfaceformat.setMajorVersion(3);
+    my_surfaceformat.setMinorVersion(3);
+    my_surfaceformat.setRedBufferSize(8);
+    my_surfaceformat.setGreenBufferSize(8);
+    my_surfaceformat.setBlueBufferSize(8);
+    my_surfaceformat.setAlphaBufferSize(8);
+    my_surfaceformat.setDepthBufferSize(24);
+    my_surfaceformat.setStencilBufferSize(8);
+    my_surfaceformat.setStereo(false);
+
+    nbody.setFormat(my_surfaceformat);
+
+    return app.exec();
+}
