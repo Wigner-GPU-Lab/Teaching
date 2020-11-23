4e6d3a64c7244f892e626711a19d0c9ebb339005
u235axe <u235axe@gmail.com>
2020-11-23 21:39:35 +0100

    Fixed cublas matmul sample code

--- GPGPU1/CUDA/culibraries/cublas.cu
+++ GPGPU1/CUDA/culibraries/cublas.cu
@@ -5,9 +5,10 @@
 #include <chrono>
 #include <iostream>
 #include <cublas_v2.h>
+#include "cpu_matmul.h"
 
- int main()
- {
+int main()
+{
     const int N = 1024;
 
     std::vector<float> A(N*N);
@@ -51,7 +52,7 @@
     float* cbA = nullptr;
     float* cbB = nullptr;
     float* cbC = nullptr;
-    
+
     cudaStatus = cudaMalloc((void**)&cbA, N*N*sizeof(float));
     if(cudaStatus != cudaSuccess){ std::cout << "Cannot allocate device memory for matrix A:" << cudaGetErrorString(cudaStatus) << "\n"; return -1; }
 
@@ -72,12 +73,13 @@
     if(cudaStatus != cudaSuccess){ std::cout << "Error recording event (0): " << cudaGetErrorString(cudaStatus) << "\n"; return -1; }
 
     // Matrix multiplication funciton: https://docs.nvidia.com/cuda/cublas/index.html#cublas-lt-t-gt-gemm
+    // All matrixes need to be transposed, including the result C, but since C^T = (AB)^T = (B^T A^T), we dont need to transpose the input matrices, just change their order:
     float one = 1.0f;
-    status = cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, N, N, N, &one, cbA, N, cbB, N, &one, cbC, N);
+    status = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N, &one, cbB, N, cbA, N, &one, cbC, N);
     if(status != CUBLAS_STATUS_SUCCESS){ std::cout << "Cannot start cublas matrix multiplication:" << status << "\n"; return -1; }
 
     cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess ){ std::cout << "CUDA error in kernel call: " << cudaGetErrorString(cudaStatus) << "\n"; return -1; }
+    if (cudaStatus != cudaSuccess ){ std::cout << "CUDA error in kernel call: " << cudaGetErrorString(cudaStatus) << "\n"; return -1; }
 
     cudaStatus = cudaEventRecord(evt[1], stream);
     if(cudaStatus != cudaSuccess){ std::cout << "Error recording event (1): " << cudaGetErrorString(cudaStatus) << "\n"; return -1; }
@@ -87,8 +89,7 @@
     float dt = 0.0f; //milliseconds
     cudaStatus = cudaEventElapsedTime(&dt, evt[0], evt[1]);
     if(cudaStatus != cudaSuccess){ std::cout << "Error getting event 0-1 elapsed time: " << cudaGetErrorString(cudaStatus) << "\n"; return -1; }
-    std::cout << "Cublas matrix multiplication took " << dt << " ms.\n";
-
+    
     status = cublasGetMatrix(N, N, sizeof(float), cbC, N, C2.data(), N);
     if(status != CUBLAS_STATUS_SUCCESS){ std::cout << "Cannot copy back results for cublas matrix C:" << status << "\n"; return -1; }
 
@@ -116,5 +117,32 @@
     status = cublasDestroy(handle);
     if(status != CUBLAS_STATUS_SUCCESS){ std::cout << "Error shutting down cublas:" << status << "\n"; return -1; }
 
+    auto t1 = std::chrono::high_resolution_clock::now();
+    cpu_matmul_improved(C1, A, B, N);
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    const float max_err = 1e-5f;
+    auto comparator = [max_err](float l, float r){ return std::abs(l-r) < max_err; };
+    
+    for(int i=0; i<N*N; ++i)
+	{
+        if( !comparator(C1[i], C2[i]) )
+        {
+            std::cout << "C1 vs C2 [" << i << "] : " << C1[i] << "   " << C2[i] << " absolute error: " << std::abs(C1[i]-C2[i]) << "\n";
+        }
+    }
+
+    if( std::equal(C1.begin(), C1.end(), C2.begin(), comparator) )
+	{
+		std::cout << "GPU improved matches CPU naive.\n";
+	}
+	else
+	{
+		std::cout << "Mismatch in CPU and GPU results.\n";
+	}
+    
+    std::cout << "CPU improved Computation took:     " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count()/1000.0f << " ms\n";
+    std::cout << "Cublas matrix multiplication took: " << dt << " ms.\n";
+
     return 0;
-  }
+}
