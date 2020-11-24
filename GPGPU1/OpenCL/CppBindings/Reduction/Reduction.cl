2da52b8c5311892a59de5105bb3857909b251dfc
Nagy-Egri Máté Ferenc <nagy-egri.mate@wigner.mta.hu>
2020-11-24 14:30:13 +0100

    Reduction finished

--- GPGPU1/OpenCL/CppBindings/Reduction/Reduction.cl
+++ GPGPU1/OpenCL/CppBindings/Reduction/Reduction.cl
@@ -1,45 +1,52 @@
+#pragma OPENCL EXTENSION cl_khr_printf: enable
+
+float read_local(local float* shared, size_t count, float zero, size_t i)
+{
+    return i < count ? shared[i] : zero;
+}
+
 kernel void reduce(
-    unsigned int length,
-    local float* shared,
     global float* front,
-    global float* back
+    global float* back,
+    local float* shared,
+    unsigned int length,
+    float zero_elem
 )
 {
-    const size_t gid = get_global_id(0),
-                 lid = get_local_id(0),
+    const size_t lid = get_local_id(0),
                  lsi = get_local_size(0),
                  wid = get_group_id(0),
                  wsi = get_num_groups(0);
 
-    const size_t wg_stride = lsi * 2;
+    const size_t wg_stride = lsi * 2,
+                 valid_count = wid != wsi - 1 ? // If not last group
+                    wg_stride :                 // as much as possible
+                    length - wid * wg_stride;   // only the remaining
 
     // Copy real data to local
     event_t read;
     async_work_group_copy(
         shared,
         front + wid * wg_stride,
-        wid != wsi - 1 ?        // If not last group
-            wg_stride :         // fill local as normal
-            length % wg_stride, // otherwise copy only available
+        valid_count,
         read);
     wait_group_events(1, &read);
-
-    // Fill rest with dummy zero elements of the group
-    if (wid == (wsi - 1))
-    {
-        if ((lsi + lid) >= (length % wg_stride))
-            shared[lsi + lid] = MAXFLOAT;
-        if (lid >= (length % wg_stride))
-            shared[lid] = MAXFLOAT;
-    }
     barrier(CLK_LOCAL_MEM_FENCE);
+
+    //if (lid == 0) printf("%d", valid_count);
+    //printf("shared[%d] = %f", lid, read_local(shared, valid_count, zero_elem, lid));
+    //barrier(CLK_LOCAL_MEM_FENCE);
+    //printf("shared[%d] = %f", lid + lsi, read_local(shared, valid_count, zero_elem, lid + lsi));
 
     for (int i = lsi; i != 0; i /= 2)
     {
         if (lid < i)
-            shared[lid] = min(shared[lid], shared[lid + i]);
-            //shared[lid] = shared[lid] < shared[lid + i] ? shared[lid] : shared[lid + i];
+            shared[lid] =
+                min(read_local(shared, valid_count, zero_elem, lid),
+                    read_local(shared, valid_count, zero_elem, lid + i)
+                );
         barrier(CLK_LOCAL_MEM_FENCE);
     }
     if (lid == 0) back[wid] = shared[0];
+    //if (lid == 0) printf("wid = %d has shared[0] = %f", wid, shared[0]);
 }
--- GPGPU1/OpenCL/CppBindings/Reduction/Reduction.cpp
+++ GPGPU1/OpenCL/CppBindings/Reduction/Reduction.cpp
@@ -7,8 +7,9 @@
 #include <random>       // std::default_random_engine, std::uniform_real_distribution
 #include <algorithm>    // std::transform
 #include <cstdlib>      // EXIT_FAILURE
+#include <execution>
 
-int main()
+int main(int, char* argv[])
 {
     try
     {
@@ -31,67 +32,105 @@ int main()
                                           std::istreambuf_iterator<char>{} } };
         program.build({ device });
 
-        auto reduce = cl::KernelFunctor<cl_uint, cl::LocalSpaceArg, cl::Buffer, cl::Buffer>(program, "reduce");
+        auto reduce = cl::KernelFunctor<cl::Buffer, cl::Buffer, cl::LocalSpaceArg, cl_uint, cl_float>(program, "reduce");
         auto wgs = reduce.getKernel().getWorkGroupInfo<CL_KERNEL_WORK_GROUP_SIZE>(device);
+
+        //std::cout << "Preferred WGS for kernel \"reduce\": " << wgs << std::endl;
+
         while (device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>() < wgs * 2 * sizeof(cl_float))
             wgs -= reduce.getKernel().getWorkGroupInfo<CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE>(device);
 
         if (wgs == 0) throw std::runtime_error{"Not enough local memory to serve a single sub-group."};
+        //std::cout << "WGS after local memory constraint of " << device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>() << " bytes: " << wgs << std::endl;
+
+        auto factor = wgs * 2;
+        // Every pass reduces input length by 'factor'.
+        // If actual size is not divisible by factor,
+        // an extra output element is produced using some
+        // number of zero_elem inputs.
+        auto new_size = [factor](const std::size_t actual)
+        {
+            return actual / factor + (actual % factor == 0 ? 0 : 1);
+        };
+        // NOTE: because one work-group produces one output
+        //       new_size == number_of_work_groups
+        auto global = [=](const std::size_t actual){ return new_size(actual) * wgs; };
 
         // Init computation
-        const std::size_t chainlength = std::size_t(std::pow(2u, 20u)); // 1M, cast denotes floating-to-integral conversion,
+        const std::size_t chainlength = std::atoi(argv[1]);//std::size_t(std::pow(2u, 20u)); // 1M, cast denotes floating-to-integral conversion,
                                                                         //     promises no data is lost, silences compiler warning
         std::vector<cl_float> vec(chainlength);
 
-        // Fill arrays with random values between 0 and 100
+        // Fill arrays with random values between 0 and FLOAT_MAX
         auto prng = [engine = std::default_random_engine{},
-                     distribution = std::uniform_real_distribution<cl_float>{ -100.0, 100.0 }]() mutable { return distribution(engine); };
+                     distribution = std::uniform_real_distribution<cl_float>{ 0, 1000.0f }]() mutable { return distribution(engine); };
 
         std::generate_n(std::begin(vec), chainlength, prng);
 
-        cl::Buffer front{ context, std::begin(vec), std::end(vec), false };
-        cl::Buffer back{ context, CL_MEM_READ_WRITE, (vec.size() / (wgs * 2) + 1) * sizeof(cl_float) };
+        cl::Buffer front{ context, std::begin(vec), std::end(vec), false },
+                   back{ context, CL_MEM_READ_WRITE, new_size(vec.size()) * sizeof(cl_float) };
 
         // Explicit (blocking) dispatch of data before launch
         cl::copy(queue, std::begin(vec), std::end(vec), front);
 
         // Launch kernels
+        auto dev_start = std::chrono::high_resolution_clock::now();
         std::vector<cl::Event> passes;
         cl_uint curr = static_cast<cl_uint>(vec.size());
-        do
+        while ( curr > 1 )
         {
-            std::cout << (curr % wgs != 0 ? ((curr / wgs) + 1) * wgs : curr) / 2 << std::endl;
-            std::cout << (curr % (wgs * 2)) << std::endl;
+            //std::cout << "Current: " << curr << std::endl;
+            //std::cout << "Global work-group size: " << global(curr) << std::endl;
+            //std::cout << "Number of work-groups: " << new_size(curr) << std::endl;
+            //std::cout << "Last group copy: " << curr - (new_size(curr) - 1) * factor << std::endl;
+
             passes.push_back(
                 reduce(
                     cl::EnqueueArgs{
                         queue,
                         passes,
-                        (curr % wgs != 0 ? ((curr / wgs) + 1) * wgs : curr) / 2,
+                        global(curr),
                         wgs
                     },
-                    curr,
-                    cl::Local(wgs * 2 * sizeof(cl_float)),
                     front,
-                    back
+                    back,
+                    cl::Local(factor * sizeof(cl_float)),
+                    curr,
+                    std::numeric_limits<cl_float>().max()
                 )
             );
 
-            curr /= static_cast<cl_uint>(wgs * 2);
-            std::swap(front, back);
+            curr = static_cast<cl_uint>(new_size(curr));
+            if (curr > 1) std::swap(front, back);
         }
-        while ( curr > 0 );
         for (auto& pass : passes) pass.wait();
+        auto dev_end = std::chrono::high_resolution_clock::now();
 
+        auto serial_start = std::chrono::high_resolution_clock::now();
         auto ref = *std::min_element(vec.cbegin(), vec.cend());
+        auto serial_end = std::chrono::high_resolution_clock::now();
+
+        auto par_start = std::chrono::high_resolution_clock::now();
+        auto ref2 = *std::min_element(std::execution::par_unseq, vec.cbegin(), vec.cend());
+        auto par_end = std::chrono::high_resolution_clock::now();
 
         // (Blocking) fetch of results
-        std::vector<cl_float> res(1);
-        cl::copy(queue, front, std::begin(res), std::end(res));
+        cl_float res;
+        cl::copy(queue, back, &res, &res + 1);
 
         // Validate (compute saxpy on host and match results)
-        if (ref != res[0]) throw std::runtime_error{ "Validation failed!" };
-
+        if (ref != res || ref2 != res)
+        {
+            std::cerr << "Ref: " << ref << "\nRes: " << res << std::endl;
+            throw std::runtime_error{ "Validation failed!" };
+        }
+        else
+        {
+            std::cout << "Device execution took: " << std::chrono::duration_cast<std::chrono::milliseconds>(dev_end - dev_start).count() << "ms." << std::endl;
+            std::cout << "Serial host execution took: " << std::chrono::duration_cast<std::chrono::milliseconds>(serial_end - serial_start).count() << "ms." << std::endl;
+            std::cout << "Parallel host execution took: " << std::chrono::duration_cast<std::chrono::milliseconds>(par_end - par_start).count() << "ms." << std::endl;
+        }
+        
     }
     catch (cl::BuildError& error) // If kernel failed to build
     {
